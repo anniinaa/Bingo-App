6e2a13d2b5409742ad04ab4c41b9e4603781c3ba
Anniina Aarnio <aarnioanniina@gmail.com>
2022-03-22 16:12:51 +0200

    Add winner row functionality, filter arrays

--- src/components/Bingo.js
+++ src/components/Bingo.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import shuffle from "shuffle-array";
 
 import characters from "../bingoTableNames";
 import { RandomButton } from "./RandomButton";
@@ -12,12 +11,43 @@ const newRandom = (currentRandomArray, allCharacters) => {
   return random;
 };
 
-export const Bingo = ({ bingoTableData }) => {
-  const [checked, setChecked] = useState([]);
+export const Bingo = ({ bingoTableData, mapDataToTable }) => {
   const [random, setRandom] = useState("");
   const [randomArray, setRandomArray] = useState([]);
   const [data, setData] = useState(bingoTableData);
   const [hasWon, setHasWon] = useState(false);
+
+  const arrayContainsElements = (arrays, filteredArray) => {
+    const results = arrays.map((x) =>
+      x.every((x) => filteredArray.map((x) => x.index).includes(x))
+    );
+
+    if (results.includes(true)) {
+      return true;
+    }
+    return false;
+  };
+
+  const checkIfWon = (dataArray) => {
+    const array1 = [
+      [0, 1, 2, 3, 4],
+      [5, 6, 7, 8, 9],
+      [10, 11, 12, 13, 14],
+      [15, 16, 17, 18, 19],
+      [20, 21, 22, 23, 24],
+      [0, 5, 10, 15, 20],
+      [1, 6, 11, 16, 21],
+      [2, 7, 12, 17, 22],
+      [3, 8, 13, 18, 23],
+      [4, 9, 14, 19, 24],
+      [0, 6, 12, 18, 24],
+      [4, 8, 12, 16, 20],
+    ];
+    const filteredArray = dataArray.filter((x) => x.selected);
+
+    const playerWon = arrayContainsElements(array1, filteredArray);
+    return playerWon;
+  };
 
   const randomCharacter = () => {
     if (randomArray.length >= characters.length) {
@@ -29,35 +59,28 @@ export const Bingo = ({ bingoTableData }) => {
     setRandomArray((oldArr) => [...oldArr, random]);
   };
 
-  const selectedButton = (e) => {
-    e.preventDefault();
+  const selectedButton = (i) => {
+    const current = data.find((x) => x.index === i);
 
-    if (e.target.value !== checked.value) {
-      setChecked((oldArr) => [...oldArr, e.target.value]);
-      e.target.style.backgroundColor = "rgba(16, 94, 26, 0.219)";
+    const filtered = data.filter((x) => x.index !== i);
+
+    current.selected = !current.selected;
+
+    setData([...filtered, current]);
+
+    if (checkIfWon([...filtered, current]) === true) {
+      setHasWon(true);
     }
   };
 
   const resetGame = () => {
-    const newData = shuffle(characters).slice(0, 25);
-
-    setData(newData);
     setRandomArray([]);
-    setChecked([]);
+
+    const newData = mapDataToTable(characters);
+    setData(newData);
     setRandom("");
     setHasWon(false);
   };
-
-  console.log("randomArray", randomArray);
-  console.log("checked", checked);
-
-  const WinnerLineCount = () => {};
-
-  // disable button kun klikattu? tallentaa saman nimen kokoajan udestaan array
-  // vahingossa klikattu saa myös poistettua arraysta
-
-  // voittologiikka 5 rivissä
-  // voitto kertojen tallennus firestoreen
 
   return (
     <div className="bingo-container">
@@ -73,19 +96,25 @@ export const Bingo = ({ bingoTableData }) => {
         </h3>
       ) : (
         <div className="bingo-wrapper">
-          {data.map((char, id) => (
-            <button
-              className="bingo-button"
-              key={id}
-              id={id}
-              onClick={selectedButton}
-              disabled={hasWon || !random}
-              value={char}
-              style={!random ? { backgroundColor: "rgb(84, 75, 109)" } : {}}
-            >
-              {char}
-            </button>
-          ))}
+          {data
+            ?.sort((a, b) => (a.index < b.index ? -1 : 1))
+            .map((char) => (
+              <button
+                className="bingo-button"
+                key={char.index}
+                id={char.index}
+                onClick={() => selectedButton(char.index)}
+                disabled={hasWon}
+                value={char.name}
+                style={
+                  !char.selected
+                    ? { backgroundColor: "rgb(84, 75, 109)" }
+                    : { backgroundColor: "rgba(16, 94, 26, 0.219)" }
+                }
+              >
+                {char.name}
+              </button>
+            ))}
         </div>
       )}
       {random ? <h2 className="characters-heading">Character list: </h2> : null}
